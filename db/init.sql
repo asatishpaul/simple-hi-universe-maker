db947a895de270ebe5d2e1107ace2ae383a2dcfe
asatishpaul <101627595+asatishpaul@users.noreply.github.com>
2025-08-17 16:47:17 +0530

    Create init.sql

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS submissions (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(150),
+  message TEXT,
+  submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
